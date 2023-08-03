9a7e6110605db4ce373bbe45a2630e1623f1c16e
VigularIgnat <ignatvigular@gmail.com>
2023-08-03 20:34:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordCard Ihnat</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="text-center flex justify-center">
+    <div class="max-w-sm w-1/2 h-full mt-6 mr-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+
+            
+            
+<h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white p-10">Select application</h5>
+
+
+<div class=" w-11/12 h-full m-auto border-2 border-grey-200 rounded-lg">
+    <a href="https://wordcard.tech/">
+    <h6 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">WordCard</h6>
+
+        <button type="butoon" class="text-white bg-violet-600 focus:ring-4 hover:bg-violet-800 focus:ring-violet-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">WordCard</button>    
+
+    </a>
+
+</div>
+<div class="mt-10 mb-10 w-11/12 h-full m-auto border-2 border-grey-200 rounded-lg">
+    <a href="https://wordcard.tech/">
+    <h6 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">RestApi</h6>
+
+<button type="button" class="text-white bg-violet-600 focus:ring-4 hover:bg-violet-800 focus:ring-violet-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">RestApi</button>    
+    </a>
+
+
+</div>
+<h7 class="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white p-10">Created by Ihnat Vyhuliar</h7>
+
+</div>
+    </div>
+
+</body>
+</html>
