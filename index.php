f6bf1389af1f180ce464d102c928d0637d346205
VigularIgnat <ignatvigular@gmail.com>
2023-08-06 13:08:50 +0200

    Changing url

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 
 <div class=" w-11/12 h-full m-auto border-2 border-grey-200 rounded-lg">
-    <a href="https://wordcard.tech/">
+    <a href="https://wordcard.tech/wordcard">
     <h6 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">WordCard</h6>
 
         <button type="butoon" class="text-white bg-violet-600 focus:ring-4 hover:bg-violet-800 focus:ring-violet-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 mt-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">WordCard</button>    
